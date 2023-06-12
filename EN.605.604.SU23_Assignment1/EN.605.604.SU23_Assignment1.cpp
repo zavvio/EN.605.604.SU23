6aca49f3b7d671842acf98b9b37b422c286f5ce8
Zavvio Mok <zavvio@gmail.com>
2023-06-11 17:08:51 -0700

    Add project files.

---
+++ EN.605.604.SU23_Assignment1/EN.605.604.SU23_Assignment1.cpp
@@ -0,0 +1,12 @@
+﻿// EN.605.604.SU23_Assignment1.cpp : Defines the entry point for the application.
+//
+
+#include "EN.605.604.SU23_Assignment1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
